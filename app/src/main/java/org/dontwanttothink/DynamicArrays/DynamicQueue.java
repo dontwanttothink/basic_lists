c2b50765d422ba0b83b5f4f413624b7d92ad9a45
dontwanttothink <git@dontwanttoth.ink>
2026-09-25 15:11:22 -0500

    Estructuras restantes

--- app/src/main/java/org/dontwanttothink/DynamicArrays/DynamicQueue.java
+++ app/src/main/java/org/dontwanttothink/DynamicArrays/DynamicQueue.java
@@ -1,33 +1,122 @@
 package org.dontwanttothink.DynamicArrays;
 
+import java.util.NoSuchElementException;
+
 public class DynamicQueue<T> implements MyQueue<T> {
+	private T[] buffer;
+	private int start = 0;
+	private int length = 0;
+
+	final private static int INITIAL_CAPACITY = 2;
+
+	@SuppressWarnings("unchecked")
+	public DynamicQueue() {
+		buffer = (T[]) new Object[INITIAL_CAPACITY];
+	}
 
 	@Override
 	public void enqueue(T x) {
+		if (isFull()) {
+			growBuffer();
+		}
+
+		buffer[(start + length) % buffer.length] = x;
+		++length;
+		length %= buffer.length;
 	}
 
 	@Override
 	public T dequeue() {
-		return null;
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+
+		T out = buffer[start];
+
+		++start;
+		start %= buffer.length;
+
+		--length;
+
+		if (length <= buffer.length / 4) {
+			shrinkBuffer();
+		}
+
+		return out;
 	}
 
 	@Override
 	public T front() {
-		return null;
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+
+		return buffer[(start + length) % buffer.length];
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return length == 0;
+	}
+
+	private boolean isFull() {
+		return length == buffer.length;
 	}
 
 	@Override
 	public int size() {
-		return 0;
+		return length;
 	}
 
 	@Override
 	public void delete(T n) {
+		for (int i = 0; i < length; ++i) {
+			int index = (start + i) % length;
+			T item = buffer[index];
+			if (item == null ? n == null : item.equals(n)) {
+				int firstChunk = Math.min(buffer.length - start, length);
+				if (i < firstChunk) {
+					System.arraycopy(buffer, index + 1, buffer, index, buffer.length - (i + 1));
+					buffer[0] = buffer[length - 1];
+					System.arraycopy(buffer, 1, buffer, 0, length - firstChunk);
+				} else {
+					System.arraycopy(buffer, index + 1, buffer, index, length - firstChunk);
+				}
+
+				--length;
+
+				if (length <= buffer.length / 4) {
+					shrinkBuffer();
+				}
+
+				return;
+			}
+		}
 	}
 
+	private void copyContiguously(T[] into) {
+		int firstChunk = Math.min(buffer.length - start, length);
+		System.arraycopy(buffer, start, into, 0, firstChunk);
+		System.arraycopy(buffer, 0, into, firstChunk, length - firstChunk);
+	}
+
+	private void shrinkBuffer() {
+		@SuppressWarnings("unchecked")
+		T[] smallerBuffer = (T[]) new Object[buffer.length / 2];
+
+		copyContiguously(smallerBuffer);
+
+		buffer = smallerBuffer;
+		start = 0;
+	}
+
+	private void growBuffer() {
+		@SuppressWarnings("unchecked")
+		T[] biggerBuffer = (T[]) new Object[buffer.length * 2];
+
+		copyContiguously(biggerBuffer);
+
+		buffer = biggerBuffer;
+		start = 0;
+	}
 }
--- app/src/main/java/org/dontwanttothink/DynamicArrays/DynamicStack.java
+++ app/src/main/java/org/dontwanttothink/DynamicArrays/DynamicStack.java
@@ -1,32 +1,89 @@
 package org.dontwanttothink.DynamicArrays;
 
+import java.util.NoSuchElementException;
+
 public class DynamicStack<T> implements MyStack<T> {
+	private T[] buffer;
+	int length = 0;
+
+	final private static int INITIAL_CAPACITY = 2;
+
+	@SuppressWarnings("unchecked")
+	public DynamicStack() {
+		buffer = (T[]) new Object[INITIAL_CAPACITY];
+	}
 
 	@Override
 	public void push(T x) {
+		if (length >= buffer.length) {
+			growBuffer();
+		}
+
+		buffer[length] = x;
+		++length;
 	}
 
 	@Override
 	public T pop() {
-		return null;
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+
+		T out = buffer[length - 1];
+		--length;
+
+		if (length <= buffer.length / 4) {
+			shrinkBuffer();
+		}
+
+		return out;
 	}
 
 	@Override
 	public T peek() {
-		return null;
+		return buffer[length - 1];
 	}
 
 	@Override
 	public boolean isEmpty() {
-		return false;
+		return length == 0;
 	}
 
 	@Override
 	public int size() {
-		return 0;
+		return length;
 	}
 
 	@Override
 	public void delete(T n) {
+		for (int i = 0; i < length; ++i) {
+			if (buffer[i] == null ? n == null : buffer[i].equals(n)) {
+				System.arraycopy(buffer, i, buffer, i + 1, length - (i + 1));
+				--length;
+
+				if (length <= buffer.length / 4) {
+					shrinkBuffer();
+				}
+
+				return;
+			}
+		}
+		throw new NoSuchElementException();
+	}
+
+	private void shrinkBuffer() {
+		@SuppressWarnings("unchecked")
+		T[] smallerBuffer = (T[]) new Object[length / 2];
+
+		System.arraycopy(buffer, 0, smallerBuffer, 0, length);
+		buffer = smallerBuffer;
+	}
+
+	private void growBuffer() {
+		@SuppressWarnings("unchecked")
+		T[] smallerBuffer = (T[]) new Object[length * 2];
+
+		System.arraycopy(buffer, 0, smallerBuffer, 0, length);
+		buffer = smallerBuffer;
 	}
 }
